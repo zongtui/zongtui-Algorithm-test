5d3788492ae81a1b2ada2a417ae4979570948dfc
cloudskyme <cloudskyme@163.com>
2015-03-30 17:18:11 +0800

    提交算法工程项目

---
+++ algorithm/src/main/java/com/zongtui/algorithm/App.java
@@ -0,0 +1,13 @@
+package com.zongtui.algorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
